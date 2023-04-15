3b013fddcdbcb7b901c238ca1d4dc4a61c3b6537
badbadballz <roy.chan22@gmail.com>
2023-04-15 19:19:13 +0800

    solutions for A7

--- solutions/A7.hs
+++ solutions/A7.hs
@@ -10,16 +10,20 @@ import System.Directory ( doesFileExist )
 
 -- Q#01
 
-getUpperChar = undefined
+getUpperChar :: IO Char
+getUpperChar = fmap toUpper getChar
 
 -- Q#02
 
-_DICT_ = undefined
+_DICT_ :: IO Dictionary
+_DICT_ = lines <$> readFile _DICT_FILE_
 
 -- Q#03
-
-makeGameIfValid = undefined
+makeGameIfValid :: Either GameException Secret -> Either GameException Game
+makeGameIfValid (Left err) = Left err
+makeGameIfValid (Right s) = Right (makeGame s) 
 
 -- Q#04
-
-getDict = undefined
+getDict :: IO (Maybe Dictionary)
+getDict = let f = toMaybe <$> doesFileExist _DICT_FILE_
+            in f <*> _DICT_
